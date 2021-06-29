9c069e1afc3d98be6040da8ccc053005d32fc24e
Manuel Pretelin <m_pretelin@hotmail.com>
2021-06-29 17:21:06 -0500

    chore(cognito): add logs on error conditions

--- src/CognitoSyncDataset.js
+++ src/CognitoSyncDataset.js
@@ -401,16 +401,22 @@ AWS.CognitoSyncManager.Dataset = (function () {
                                         root.logger('No conflicts. Updating local records.');
 
                                         root.local.putRecords(root.getIdentityId(), root.datasetName, updatedRemoteRecords, function (err) {
-                                            root.logger('putRecords: callback: error ' + Boolean(err) && err.message);
+                                            root.logger('putRecords: callback: error ' + Boolean(err));
 
-                                            if (err) { return callback.onFailure(err); }
+                                            if (err) {
+                                                root.logger('putRecords: callback: error message' + err);
+                                                return callback.onFailure(err); 
+                                            }
 
                                             // Update the local sync count to match.
 
                                             root.local.updateLastSyncCount(root.getIdentityId(), root.datasetName, lastSyncCount, function (err) {
-                                                root.logger('updateLastSyncCount: callback: error ' + Boolean(err) && err.message);
+                                                root.logger('updateLastSyncCount: callback: error ' + Boolean(err));
 
-                                                if (err) { return callback.onFailure(err); }
+                                                if (err) {
+                                                    root.logger('updateLastSyncCount: callback: error message' + err);
+                                                    return callback.onFailure(err); 
+                                                }
 
                                                 root.logger('Finished resolving records. Restarting sync.');
 
